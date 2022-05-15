fa36198e6f7e9de34d76130c8f19930d3a2778f8
RauMax <maximilianoerau@gmail.com>
2022-05-15 10:09:01 -0300

    Hex function on development

--- app.js
+++ app.js
@@ -29,3 +29,49 @@ function reset(){
 function setBg(color){
     backGround.style.backgroundColor = color;
 }
+
+function randomize(){
+    let arr = []
+    let exaColor = ""
+    for(let i = 0; i < 6; i++){
+        let randomNumber = Math.floor(Math.random() * 16)
+        if(randomNumber === 10){
+            arr.push("A")
+            exaColor += "A"
+        }
+        else if(randomNumber === 11){
+            arr.push("B")
+            exaColor += "B"
+        }
+        else if(randomNumber === 12){
+            arr.push("C")
+            exaColor += "C"
+        }
+        else if(randomNumber === 13){
+            arr.push("D")
+            exaColor += "D"
+        }
+        else if(randomNumber === 14){
+            arr.push("E")
+            exaColor += "E"
+        }
+        else if(randomNumber === 15){
+            arr.push("F")
+            exaColor += "F"
+        }
+        else {
+            arr.push(randomNumber)
+            exaColor += randomNumber
+        }
+        exaColor += randomNumber
+    }
+    console.log(arr)
+    console.log(exaColor)
+    let finalColor = ""
+    for(let i = 0; i <= 5 ; i++){
+        finalColor += arr[i]
+    }
+    console.log(finalColor)
+}
+
+randomize()
--- hex.js
+++ hex.js
@@ -1,1 +1,47 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn")
+const idSpan = document.getElementById("color-name")
+const backGround = document.getElementById("back-ground")
+
+
+btn.addEventListener("click", function(){
+    let arr = []
+    for(let i = 0; i < 6; i++){
+        let randomNumber = Math.floor(Math.random() * 16)
+        if(randomNumber === 10){
+            arr.push("A")
+        }
+        else if(randomNumber === 11){
+            arr.push("B")
+        }
+        else if(randomNumber === 12){
+            arr.push("C")
+        }
+        else if(randomNumber === 13){
+            arr.push("D")
+        }
+        else if(randomNumber === 14){
+            arr.push("E")
+        }
+        else if(randomNumber === 15){
+            arr.push("F")
+        }
+        else {
+            arr.push(randomNumber)
+        }
+    }
+    console.log(arr)
+    let finalColor = ""
+    for(let i = 0; i <= 5 ; i++){
+        finalColor += arr[i]
+    }
+    console.log(finalColor)
+    setBg(finalColor)
+})
+
+
+function setBg(color){
+    backGround.style.backgroundColor = `#${color}`;
+    console.log(`#${color}`)
+    idSpan.textContent = `#${color}`
+}
